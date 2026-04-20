e44340bc691938beae5a249920d4033ec41e9a2c
GitHub Action <action@github.com>
2026-04-20 15:02:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/ashrex05oss/autocomplete/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ashrex05oss/autocomplete/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ashrex05oss.autocomplete.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ashrex05oss.autocomplete.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/ashrex05oss/autocomplete/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ashrex05oss/autocomplete/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ashrex05oss.autocomplete.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
